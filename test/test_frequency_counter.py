3512dedf02ad241aa2b83bfd9aacb4d60f078e61
Matt Venn <matt@mattvenn.net>
2021-10-21 12:54:26 +0200

    fixup for new cocotb

--- test/test_frequency_counter.py
+++ test/test_frequency_counter.py
@@ -5,17 +5,17 @@ from test_seven_segment import read_segments
 import random
 
 async def reset(dut):
-    dut.reset  <= 1
-    dut.period_load <= 0
+    dut.reset.value = 1
+    dut.period_load.value = 0
     await ClockCycles(dut.clk, 5)
-    dut.reset <= 0;
+    dut.reset.value = 0;
     await ClockCycles(dut.clk, 5)
 
 async def update_period(dut, period):
-    dut.period_load <= 1
-    dut.period <= period
+    dut.period_load.value = 1
+    dut.period.value = period
     await ClockCycles(dut.clk, 1)
-    dut.period_load <= 0
+    dut.period_load.value = 0
     await ClockCycles(dut.clk, 1)
 
 @cocotb.test()
@@ -31,7 +31,7 @@ async def test_load(dut):
 async def test_all(dut):
     clock_mhz = 12
     clk_period_ns = round(1/clock_mhz * 1000, 2)
-    dut.log.info("input clock = %d MHz, period = %.2f ns" %  (clock_mhz, clk_period_ns))
+    dut._log.info("input clock = %d MHz, period = %.2f ns" %  (clock_mhz, clk_period_ns))
 
     clock = Clock(dut.clk, clk_period_ns, units="ns")
     cocotb.fork(clock.start())
@@ -44,7 +44,7 @@ async def test_all(dut):
     for input_freq in [10, 69, 90]:
         # create an input signal
         period_us = round((1/input_freq) * 100, 2)
-        dut.log.info("input freq = %d kHz, period = %.2f us" %  (input_freq, period_us))
+        dut._log.info("input freq = %d kHz, period = %.2f us" %  (input_freq, period_us))
         input_signal = cocotb.fork(Clock(dut.signal, period_us,  units="us").start())
 
         # give it 4 update periods to allow counters to adjust
--- test/test_seven_segment.py
+++ test/test_seven_segment.py
@@ -4,10 +4,10 @@ from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles, with_timeout, 
 import random
 
 async def reset(dut):
-    dut.reset  <= 1
+    dut.reset.value = 1
     await ClockCycles(dut.clk, 5)
-    dut.reset <= 0;
-    dut.load <= 0;
+    dut.reset.value = 0;
+    dut.load.value = 0;
     await ClockCycles(dut.clk, 5)
 
 segments = {
@@ -31,7 +31,7 @@ async def read_segments(dut):
     await ReadWrite() # wait for combinatorial output to settle
     units = segments[int(dut.segments)]
     number = tens * 10 + units
-    dut.log.debug("segments show %02d" % number)
+    dut._log.debug("segments show %02d" % number)
     return number
 
 @cocotb.test()
@@ -44,10 +44,10 @@ async def test_seven_segment(dut):
 
     for tens in range(10):
         for units in range(10):
-            dut.ten_count <= tens
-            dut.unit_count <= units
-            dut.load <= 1
+            dut.ten_count.value = tens
+            dut.unit_count.value= units
+            dut.load.value = 1
             await ClockCycles(dut.clk, 1)
-            dut.load <= 0
+            dut.load.value = 0
             await ClockCycles(dut.clk, 2) # have to wait a couple of cycles for flops
             assert await read_segments(dut) == tens * 10 + units
