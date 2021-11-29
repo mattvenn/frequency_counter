4d42b40ae5e99ab874151cafed4881792c2b9aee
Matt Venn <matt@mattvenn.net>
2021-11-29 15:47:22 +0100

    update tests from main

--- test/test_frequency_counter.py
+++ test/test_frequency_counter.py
@@ -1,21 +1,21 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
-from test.test_seven_segment import read_segments
+from test_seven_segment import read_segments
 import random
 
 async def reset(dut):
-    dut.reset  <= 1
-    dut.period_load <= 0
+    dut.reset.value = 1
+    dut.period_load.value = 0
     await ClockCycles(dut.clk, 5)
-    dut.reset <= 0;
+    dut.reset.value = 0;
     await ClockCycles(dut.clk, 5)
 
 async def update_period(dut, period):
-    dut.period_load <= 1
-    dut.period <= period
+    dut.period_load.value = 1
+    dut.period.value = period
     await ClockCycles(dut.clk, 1)
-    dut.period_load <= 0
+    dut.period_load.value = 0
     await ClockCycles(dut.clk, 1)
 
 @cocotb.test()
@@ -31,10 +31,10 @@ async def test_load(dut):
 async def test_all(dut):
     clock_mhz = 12
     clk_period_ns = round(1/clock_mhz * 1000, 2)
-    dut.log.info("input clock = %d MHz, period = %.2f ns" %  (clock_mhz, clk_period_ns))
+    dut._log.info("input clock = %d MHz, period = %.2f ns" %  (clock_mhz, clk_period_ns))
 
     clock = Clock(dut.clk, clk_period_ns, units="ns")
-    cocotb.fork(clock.start())
+    clock_sig = cocotb.fork(clock.start())
     await reset(dut)
    
     # adjust the update period to match clock freq
@@ -43,13 +43,15 @@ async def test_all(dut):
     
     for input_freq in [10, 69, 90]:
         # create an input signal
-        period_us = round((1/input_freq) * 100, 2)
-        dut.log.info("input freq = %d kHz, period = %.2f us" %  (input_freq, period_us))
+        period_us = round((1/input_freq) * 100, 3)
+        dut._log.info("input freq = %d kHz, period = %.2f us" %  (input_freq, period_us))
         input_signal = cocotb.fork(Clock(dut.signal, period_us,  units="us").start())
 
-        # give it 3 update periods to allow counters to adjust
-        await ClockCycles(dut.clk, period * 3)
+        # give it 4 update periods to allow counters to adjust
+        await ClockCycles(dut.clk, period * 4)
         assert await read_segments(dut) == input_freq
 
         # kill signal
         input_signal.kill()
+
+    clock_sig.kill()
--- test/test_seven_segment.py
+++ test/test_seven_segment.py
@@ -1,13 +1,13 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles, with_timeout
+from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles, with_timeout, Timer, ReadWrite
 import random
 
 async def reset(dut):
-    dut.reset  <= 1
+    dut.reset.value = 1
     await ClockCycles(dut.clk, 5)
-    dut.reset <= 0;
-    dut.load <= 0;
+    dut.reset.value = 0;
+    dut.load.value = 0;
     await ClockCycles(dut.clk, 5)
 
 segments = {
@@ -24,12 +24,14 @@ segments = {
     }
 
 async def read_segments(dut):
-    await with_timeout(FallingEdge(dut.digit), 100, 'us')
-    tens = segments[int(dut.segments)]
     await with_timeout(RisingEdge(dut.digit), 100, 'us')
+    await ReadWrite() # wait for combinatorial output to settle: https://github.com/cocotb/cocotb/wiki/Timing-Model#readwrite
+    tens = segments[int(dut.segments)]
+    await with_timeout(FallingEdge(dut.digit), 100, 'us')
+    await ReadWrite() # wait for combinatorial output to settle
     units = segments[int(dut.segments)]
     number = tens * 10 + units
-    dut.log.debug("segments show %02d" % number)
+    dut._log.debug("segments show %02d" % number)
     return number
 
 @cocotb.test()
@@ -42,10 +44,10 @@ async def test_seven_segment(dut):
 
     for tens in range(10):
         for units in range(10):
-            dut.ten_count <= tens
-            dut.unit_count <= units
-            dut.load <= 1
+            dut.ten_count.value = tens
+            dut.unit_count.value= units
+            dut.load.value = 1
             await ClockCycles(dut.clk, 1)
-            dut.load <= 0
+            dut.load.value = 0
             await ClockCycles(dut.clk, 2) # have to wait a couple of cycles for flops
             assert await read_segments(dut) == tens * 10 + units
